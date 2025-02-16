27fc4d2fa8267708e64a8322eeb15e5f03f4721c
dennis dentrix <denkyusya@gmail.com>
2025-02-16 14:30:42 +0300

    refactor: update Vite configuration for automatic JSX runtime and enable sourcemaps

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic', 
+      babel: { plugins: [['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]] },
+    }),
+  ],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000, // Adjust chunk size limit
-  }
+    sourcemap: true, 
+  },
 })
