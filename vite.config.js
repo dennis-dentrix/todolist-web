7b89fc176ccc4978765d0ed015b546bd071d098b
dennis dentrix <denkyusya@gmail.com>
2025-02-21 00:32:30 +0300

    fix: update Vite configuration to specify multiple entry points for the build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
 })
