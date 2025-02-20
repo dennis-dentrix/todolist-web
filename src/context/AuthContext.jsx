8b9928ec56da6f38f55497a0a255686b89637a94
dennis dentrix <denkyusya@gmail.com>
2025-02-20 18:59:33 +0300

    fix: add logging for token type in reset password function

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -165,6 +165,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   const resetPassword = async (token, password, passwordConfirm) => {
+    console.log(typeof token, token);
     setLoading(true);
     try {
       const response = await api.patch(`/users/resetPassword/${token}`, {
